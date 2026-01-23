63c1a7061291b5e47db71b50aa99358561f9e9dc
bearsdotzone <bears@bears.zone>
2026-01-23 09:47:52 -0600

    Fix pricegun value

--- src/lib.ts
+++ src/lib.ts
@@ -2,7 +2,7 @@ import { Item, visitUrl } from "kolmafia";
 
 export function pricegunValue(item: Item): number {
   const pricegunData = visitUrl(`https://pricegun.loathers.net/api/${item.id}`);
-  const data = JSON.parse(pricegunData) as { value: number };
-  if (!data || !data.value || data.value <= 0) return 0;
-  return data.value;
+  const data = JSON.parse(pricegunData) as { value: string };
+  if (!data || !data.value || parseInt(data.value) <= 0) return 0;
+  return parseInt(data.value);
 }
